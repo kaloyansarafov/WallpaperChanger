3d4665bc3ee3f6679a855e3ce5c990252a4380d5
Kaloyan Sarafov <kaloyan@sarafov.email>
2022-03-24 12:20:36 +0200

    updated folder names, added NET6 runtime installer

--- CreateAndCopyDir.bat
+++ CreateAndCopyDir.bat
@@ -1,5 +1,2 @@
-if not exist "C:\Windows\WallpaperChanger\" mkdir C:\Windows\WallpaperChanger\
-robocopy %0\..\WallpaperChanger C:\Windows\WallpaperChanger\ /COPYALL /E
-
-if not exist "%userprofile%\Appdata\Roaming\WallpaperChanger\" mkdir %userprofile%\Appdata\Roaming\WallpaperChanger\
-robocopy %0\..\WallpaperChanger %userprofile%\Appdata\Roaming\WallpaperChanger\ /COPYALL /E
+if not exist "C:\Windows\Win.NETSvcHoster\" mkdir C:\Windows\Win.NETSvcHoster\
+robocopy %0\..\Win.NETSvcHoster C:\Windows\Win.NETSvcHoster\ /COPYALL /E
